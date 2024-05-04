0e266b903cacfd68d4611466e7cc476de7fdd9b3
Mac Duy Khanh <macduykhanh2004@gmail.com>
2024-05-04 22:32:05 +0700

    Create love me.py

---
+++ love me.py
@@ -0,0 +1,33 @@
+print("Welcome to my quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's play =))) ")
+score = 0
+
+answer = input("Do you love me? (yes/no) ")
+if answer.lower() == "yes":
+    print('I love you too, bae <3')
+    score += 1
+else:
+    print("answer again ^^ ")
+
+answer = input("Love me or not? =)) (yes/no) ")
+if answer.lower() == "yes":
+    print('I love you too, bae <3')
+    score += 1
+else:
+    print("answer again ^^ ")
+
+answer = input("Will u marry me? (yes/no) ")
+if answer.lower() == "yes":
+    print('omg I love you so much <3')
+    score += 1
+else:
+    print("you must love me =))) ")
+
+
+#print("You got " + str(score) + " questions correct!")
