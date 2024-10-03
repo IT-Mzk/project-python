7fed142c2153dc3a5390ffeffdf32ef113e13ee7
Mac Duy Khanh <macduykhanh2004@gmail.com>
2024-10-03 09:48:15 +0700

    study

---
+++ sum input number.py
@@ -0,0 +1,9 @@
+try:
+    print("input number a")
+    a= int(input())
+    print("input number b")
+    b= int(input())
+    c= a+b
+    print( "a+b=", c )
+except:
+    print("you are not input number =)))")
